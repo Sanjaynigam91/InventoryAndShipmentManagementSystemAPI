85c38d87e04841104f31248c5de77952e457ab7e
Sanjaynigam91 <nigamsanjay2022@gmail.com>
2025-03-01 18:51:31 +0530

    Latest Code Change
    Latest Code Change

--- InventoryDTO/ProductShipmentResponse.cs
+++ InventoryDTO/ProductShipmentResponse.cs
@@ -2,11 +2,19 @@
 {
     public class ProductShipmentResponse
     {
-        public int ProductId {  get; set; }
+        public int StatusCode { get; set; }
+        public bool Status { get; set; }
+        public string ResponseMessage { get; set; } = string.Empty;
+        public List<ProductShipment> Data { get; set; }
+
+    }
+    public class ProductShipment
+    {
+        public int ProductId { get; set; }
         public string ProductName { get; set; } = string.Empty;
-        public int ShipmentId {  get; set; }
-        public string ShipmentName { get; set;} = string.Empty;
+        public int ShipmentId { get; set; }
+        public string ShipmentName { get; set; } = string.Empty;
         public DateTime ShipmentDate { get; set; }
-        public int Quantity {  get; set; }
+        public int Quantity { get; set; }
     }
 }
