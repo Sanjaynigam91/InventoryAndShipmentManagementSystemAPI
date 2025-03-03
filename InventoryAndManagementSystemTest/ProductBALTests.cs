fd16beaa36bcc473c4c72312d9a20701b3d10e2c
Sanjaynigam91 <nigamsanjay2022@gmail.com>
2025-03-03 17:34:30 +0530

    Latest Coe Changes for SQL Script , Product and Shipments Data
    Latest Coe Changes for SQL Script , Product and Shipments Data

--- InventoryAndManagementSystemTest/ProductBALTests.cs
+++ InventoryAndManagementSystemTest/ProductBALTests.cs
@@ -8,14 +8,9 @@ using LISCareDataAccess.IInventoryDbContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace InventoryAndManagementSystemTest
 {
